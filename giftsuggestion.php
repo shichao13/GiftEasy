55a2b5eec357039f8552e824b2b83f96d3a9650b
Allen Chen <allenbemis777@gmail.com>
2013-12-07 00:12:12 -0500

    Optimizing Code, Midpoint

--- giftsuggestion.php
+++ giftsuggestion.php
@@ -2,361 +2,168 @@
 
 include('aws_signed_request.php');
 
-//Enter your IDs
+// IDs for pulling goods
 define("Access_Key_ID", "AKIAIMCFHBE6JELLM3JQ");
-define("SECRET_KEY","PGKAUCkEraUBFxrNFoBTf5dhE8LSFNEm+Pq1oxAd");
-
-//mySQL table is called 'Search Results'
-//INSERT INTO 'Search Results' VALUES (title, author, price, reviews)
-  
-function compareprintSearchResults($parsed_xml, $parsed_xmltwo, $SearchIndex)
-{
-  $failcount = 0;
-  print_r("<table>");
-  $numOfItems = 0;
-  $secOfItems = 0;
-  foreach($parsed_xml->Items->Item as $counting)
-          {
-            $numOfItems = $numOfItems + 1;
-          }
-  foreach($parsed_xmltwo->Items->Item as $counting)
-          {
-            $secOfItems = $secOfItems + 1;
-          }
-
-  if($numOfItems>0 && $secOfItems>0)
-  {
-    print_r($numOfItems." ".$secOfItems);
-    foreach($parsed_xmltwo->Items->Item as $compare)
-    {
-      foreach($parsed_xml->Items->Item as $current)
-      {
-        if ($current->ItemAttributes->Title == $compare->ItemAttributes->Title)
-        {
-          print_r("<td><font size='-1'><b>".$current->ItemAttributes->Title."</b>");
-          if (isset($current->ItemAttributes->Title)) 
-          {
-            print_r("<br>Title: ".$current->ItemAttributes->Title);
-          } 
-          elseif(isset($current->ItemAttributes->Author)) 
-          {
-            print_r("<br>Author: ".$current->ItemAttributes->Author);
-          } 
-          elseif(isset($current->Offers->Offer->Price->FormattedPrice))
-          {
-            print_r("<br>Price:".$current->Offers->Offer->Price->FormattedPrice);
-          }
-          else
-          {
-            print_r("<center>No matches found.</center>");
-          }
-        }
-        else
-        {
-          //print_r("\n Relevance and Bestselling do not match for item ".$current->ItemAttributes->Title."!");
-          //print_r($failcount);
-          //$failcount = $failcount + 1;
-        }
-      }
-    }
-  }
-  else
-  {
-     print_r("There are no items that match the search results!");
-     print_r("\n".$numOfItems);
-     print_r("\n".$secOfItems);
-  }
-}  
+define("SECRET_KEY","PGKAUCkEraUBFxrNFoBTf5dhE8LSFNEm+Pq1oxAd"); 
 
 function pulltitles($parsed_xml)
 {
-  $i=0;
+  // Initialize our output array
+  $output = array();
+
+  // Find output!
   foreach($parsed_xml->Items->Item as $current)
   {
-    if($i == 0)
-    {
-      $output[] = (string)$current->ItemAttributes->Title;
-      $i++;
-    }
-    else
-    {
       array_push($output, (string)$current->ItemAttributes->Title);
   }
-}
-  //print_r("Hi.");
-  //print_r($output);
+
+// Return what we have
 return $output;
 }
 
-function printtitles($parsed_xml, $searcharray, $i, $j)
+function printtitles(&$relevancexml, $searcharray, $j)
 {
+  // How many have we currently found?
   $displayedinfo = 0;
-  print_r("<table>");
-  foreach($parsed_xml->Items->Item as $current)
+
+  // How long is the array we are searching through?
+  $length = count($searcharray);
+
+  // Go through all of the text and determine if we have found matches
+  for ($i = 0, $i<$j, $i++)
   {
-    $length = count($searcharray);
-    for($j = 0; $j<$length; $j++)
+    foreach($relevancexml[$i]->Items->Item as $current)
     {
-      $comparetitle = $searcharray[$j];
-      if($current->ItemAttributes->Title == $comparetitle)
+      for($k = 0; $k<$length; $k++)
       {
-        $displayedinfo++;
-        /*print_r("<td><fontsize='-1'><b>".$current->ItemAttributes->Title."</b>");
-                if (isset($current->ItemAttributes->Title)) 
-                {
-                    print_r("<br>Title: ".$current->ItemAttributes->Title);
-                } 
-                elseif(isset($current->ItemAttributes->Author)) 
-                {
-                    print_r("<br>Author: ".$current->ItemAttributes->Author);
-                } 
-                elseif(isset($current->Offers->Offer->Price->FormattedPrice))
-                {
-                print_r("<br>Price:".$current->Offers->Offer->Price->FormattedPrice);
-                }
-        */
-        /*
-        // old query stuff
-        $review = 0.00;
-        $query = sprintf("INSERT INTO Results VALUES (%i, %s, %s, %.2f, %.2f)", 
-        $displayedinfo, 
-        (string)$current->$ItemAttributes->Title, 
-        (string)$current->$ItemAttributes->$Author, 
-        (float)$current->Offers->Offer->Price->FormattedPrice, 
-        $review);
+        // The title we are comparing to the item that we have found
+        $comparetitle = $searcharray[$k];
 
-        $result = mysql_query($query);
-        if (!result) 
+        // If they are the same, then...
+        if($current->ItemAttributes->Title == $comparetitle)
         {
-            $message = 'Invalid query: '. mysql_error() . "<br>";
-            print_r($message);
-        }
-        */
-        array_push($_SESSION['Title'], (string)$current->$ItemAttributes->Title);
-        array_push($_SESSION['Author'], (string)$current->$ItemAttributes->$Author); 
-        array_push($_SESSION['Price'], (float)$current->Offers->Offer->Price->FormattedPrice); 
-        array_push($_SESSION['Review'], $review);
-      }
-    }
-  }
-  
-  $numberremain = 10 - $displayedinfo;
-  nomatcheserror($parsed_xml, $numberremain, $displayedinfo);
-}
-function nomatcheserror($parsed_xml, $numberremain, $displayedinfo)
-{
-    print_r("No Matches Between Bestselling and Relevance were Found<br>");
-    $Operation = "ItemSearch";
-$Version = "2011-08-01";
-$ResponseGroup = "ItemAttributes,Offers";
-if ($_POST["age"] == 1)
-{
-  $SearchIndex = "Baby";
-  $Sort = "salesrank";
-}
-else if ($_POST["age"] == 2)
-{
-  $SearchIndex = "Toys";
-  $Sort = "salesrank";
-}
-else
-{
-  $SearchIndex = "All";
-}
-
-/*
-$Time = date("c");
-$Sig = base64_encode(hash_hmac('sha256', $Operation.$Time, SECRET_KEY, true));
-$Sig = str_replace('+','%2B',$Sig);
-$Sig = str_replace('=','%3D',$Sig);
-*/
-  //Define the request
-  /*$request=
-     "http://webservices.amazon.com/onca/xml"
-   . "?Service=AWSECommerceService"
-   . "&AWSAccessKeyId=" . Access_Key_ID
-   . "&Operation=" . $Operation
-   . "&Version=" . $Version
-   . "&SearchIndex=" . $SearchIndex
-   . "&Keywords=" . $Keywords
-   . "&Signature=" . $Sig
-   . "&Timestamp=" . $Time
-   . "&ResponseGroup=" . $ResponseGroup;
- */
-
-// Define Parameters Matrix
-$params['Operation']=$Operation;
-$params['SearchIndex']=$SearchIndex;
-$params['Keywords']=$Keywords;
-  //$params['ResponseGroup']=$ResponseGroup;  
-  $response = file_get_contents(aws_signed_request('com', $params, Access_Key_ID, SECRET_KEY));
-  //$response = file_get_contents($request);
-  $parsed_xml = simplexml_load_string($response);
-  
-  print_r("<table>");
-  $numOfItems = 0;
-  $covered = 0;
-  foreach($parsed_xml->Items->Item as $counting)
-          {
-            $numOfItems = $numOfItems + 1;
-          }
-  if($numOfItems >0)
-  {
-     foreach($parsed_xml->Items->Item as $current)
-             {
-               if($covered <= $numberremain)
-               {
-               $covered++;
-               /*
-               print_r("<td><font size='-1'><b>".$current->ItemAttributes->Title."</b>");
-          if (isset($current->ItemAttributes->Title)) 
-          {
-            print_r("<br>Title: ".$current->ItemAttributes->Title);
-          } 
-          elseif(isset($current->ItemAttributes->Author)) 
-          {
-            print_r("<br>Author: ".$current->ItemAttributes->Author);
-          } 
-          elseif(isset($current->Offers->Offer->Price->FormattedPrice))
-          {
-            print_r("<br>Price:".$current->Offers->Offer->Price->FormattedPrice);
-          }
-          */
-          /*
-          // old query stuff
-          $review = 0.00;
+          // We have found an extra term!
           $displayedinfo++;
-          $query = sprintf("INSERT INTO Results VALUES (%i, %s, %s, %.2f, %.2f)", 
-          $displayedinfo, 
-          (string)$current->$ItemAttributes->Title, 
-          (string)$current->$ItemAttributes->$Author, 
-          (float)$current->Offers->Offer->Price->FormattedPrice, 
-          $review);
 
-          $result = mysql_query($query);
-          if (!result) 
-          {
-              $message = 'Invalid query: '. mysql_error() . "<br>";
-              print_r($message);
-          }
-          */
-
+          // Push these arrays onto the superglobal for the next page
           array_push($_SESSION['Title'], (string)$current->$ItemAttributes->Title);
           array_push($_SESSION['Author'], (string)$current->$ItemAttributes->$Author); 
           array_push($_SESSION['Price'], (float)$current->Offers->Offer->Price->FormattedPrice); 
           array_push($_SESSION['Review'], $review);
-
-             }
-             } 
+        }
+      }
+    }
   }
+  
+  // Find out how many 
+  $numberremain = 10 - $displayedinfo;
+  nomatcheserror(&$relevancexml, $numberremain);
+}
+
+function nomatcheserror(&$relevancexml, $numberremain)
+{
+  $numOfItems = 0;
+  foreach($parsed_xml->Items->Item as $counting)
+    {
+      $numOfItems = $numOfItems + 1;
+    }
+  if($numOfItems >0)
+    {
+      $numOfItems = 0;
+      foreach ($parsed_xml->Items->Item as $current)
+      {
+        if($covered <= $numOfItems)
+        {
+          $covered++;
+          array_push($_SESSION['Title'], (string)$current->$ItemAttributes->Title);
+          array_push($_SESSION['Author'], (string)$current->$ItemAttributes->$Author); 
+          array_push($_SESSION['Price'], (float)$current->Offers->Offer->Price->FormattedPrice); 
+          array_push($_SESSION['Review'], $review);
+        }
+      } 
+    }
 }
   
 //Set up the operation in the request
-function ItemSearch($SearchIndex, $Keywords, $j, $determine){
-$Operation = "ItemSearch";
-$Version = "2011-08-01";
-$ResponseGroup = "ItemAttributes,Offers";
-if ($_POST["age"] == 1)
+function ItemSearch($SearchIndex, $Keywords, $j)
 {
-  $SearchIndex = "Baby";
-  $Sort = "salesrank";
-}
-else if ($_POST["age"] == 2)
-{
-  $SearchIndex = "Toys";
-  $Sort = "salesrank";
-}
-else
-{
-  $SearchIndex = "All";
-}
-$Keywords = $_POST["keywords"];
+  // Set parameters that will not change over time
+  $Operation = "ItemSearch";
+  $Version = "2011-08-01";
+  $ResponseGroup = "ItemAttributes,Offers";
 
-// Define Parameters Matrix
-$params['Operation']=$Operation;
-$params['SearchIndex']=$SearchIndex;
-$params['Keywords']=$Keywords;
-
-// Make userinfo into a superglobal
-$_SESSION['AmazonCategory'] = $SearchIndex;
-$_SESSION['Keywords'] = $Keywords;
-$_SESSION['Gender'] = $_POST['gender'];
-
-//array of results
-$_SESSION['Title'] = array();
-$_SESSION['Author'] = array();
-$_SESSION['Price'] = array();
-$_SESSION['Review'] = array();
-
-for($i = 1; $i<=$j; $i++)
-{
-//Set the values for some of the parameters
-
-/*
-$Time = date("c");
-$Sig = base64_encode(hash_hmac('sha256', $Operation.$Time, SECRET_KEY, true));
-$Sig = str_replace('+','%2B',$Sig);
-$Sig = str_replace('=','%3D',$Sig);
-*/
-  //Define the request
-  /*$request=
-     "http://webservices.amazon.com/onca/xml"
-   . "?Service=AWSECommerceService"
-   . "&AWSAccessKeyId=" . Access_Key_ID
-   . "&Operation=" . $Operation
-   . "&Version=" . $Version
-   . "&SearchIndex=" . $SearchIndex
-   . "&Keywords=" . $Keywords
-   . "&Signature=" . $Sig
-   . "&Timestamp=" . $Time
-   . "&ResponseGroup=" . $ResponseGroup;
- */
-
-
-  //$params['ResponseGroup']=$ResponseGroup;
-$params['ItemPage']=$i;
-  if ($i != 1)
+  // From the input, which age group are we looking at?
+  if ($_POST["age"] == 1)
   {
-    unset($params['Sort']);
+    $SearchIndex = "Baby";
+    $Sort = "salesrank";
   }
-  //Catch the response in the $response object
-  $response = file_get_contents(aws_signed_request('com', $params, Access_Key_ID, SECRET_KEY));
-  //$response = file_get_contents($request);
-  $parsed_xml = simplexml_load_string($response);
-$params['Sort']=$Sort;
-$responsetwo = file_get_contents(aws_signed_request('com', $params, Access_Key_ID, SECRET_KEY));
-$parsed_xmltwo = simplexml_load_string($responsetwo);
+  else if ($_POST["age"] == 2)
+  {
+    $SearchIndex = "Toys";
+    $Sort = "salesrank";
+  }
+  else
+  {
+    $SearchIndex = "All";
+  }
 
-if($determine == 1)
-{
-if ($i == 1)
-{
-  // print_r($parsed_xmltwo);
-      $relevancearray = pulltitles($parsed_xml);
-      $bestsellarray = pulltitles($parsed_xmltwo);
-}
-else
-{
+  // Other user input not related to age
+  $Keywords = $_POST["keywords"];
+
+  // Define Parameters Matrix
+  $params['Operation']=$Operation;
+  $params['SearchIndex']=$SearchIndex;
+  $params['Keywords']=$Keywords;
+
+  // Make userinfo into a superglobal
+  $_SESSION['AmazonCategory'] = $SearchIndex;
+  $_SESSION['Keywords'] = $Keywords;
+  $_SESSION['Gender'] = $_POST['gender'];
+
+  // Array of results
+  $_SESSION['Title'] = array();
+  $_SESSION['Author'] = array();
+  $_SESSION['Price'] = array();
+  $_SESSION['Review'] = array();
+
+  // Begin iterating over number of pages
+  for($i = 1; $i<=$j; $i++)
+  {
+    //Which Item Page are we on?
+    $params['ItemPage']=$i;
+
+    // If it isn't the first one, then we don't want sort to be in it
+    if ($i != 1)
+    {
+      unset($params['Sort']);
+    }
+
+    // Create a response with proper signatures using the other function, get from Amazon
+    $response = file_get_contents(aws_signed_request('com', $params, Access_Key_ID, SECRET_KEY));
+    array_push($relevancexml, simplexml_load_string($response));
+
+    // Create a second response with the sort on bestseller instead
+    $params['Sort'] = $Sort;
+    $responsetwo = file_get_contents(aws_signed_request('com', $params, Access_Key_ID, SECRET_KEY));
+    array_push($bestsellxml, simplexml_load_string($responsetwo));
+
+    // Start pulling titles into the total titles array
     $relevancearray = array_merge($relevancearray, pulltitles($parsed_xml));
     $bestsellarray = array_merge($bestsellarray, pulltitles($parsed_xmltwo));
-    if ($i == $j-1)
-    {
-      $determine = 0;
-    }
-}
-}
-else
-{
-  printtitles($parsed_xml, $bestsellarray, $i, $j);
-//compareprintSearchResults($parsed_xml, $parsed_xmltwo, $SearchIndex);
-}
-}
+  }
+    
+  printtitles(&$relevancexml, $bestsellarray, $j);
+
 }
 
 session_start();
-$Keywords = $_POST["keywords"];
 $determine = 1;
 $j=6;
+
+// Defining the arrays of titles
+$relevancearray = array();
+$bestsellarray = array();
 
 $relevancexml = array();
 $bestsellxml = array();
